10a6b425909cba998026e7218dc38508a31f80b1
palinakirpichonak-arch <palina.kirpichonak@innowise.com>
2025-10-21 12:00:55 +0300

    [lang-43]fix: rename llm client

--- MainService/MainService.AL/Features/LLM/LlmService.cs
+++ MainService/MainService.AL/Features/LLM/LlmService.cs
@@ -1,14 +1,17 @@
 ﻿using MainService.BLL.Services.LLM;
+using MainService.BLL.Services.UnitOfWork;
 
 namespace MainService.AL.Features.LLM;
 
 public class LlmService : ILlmService
 {
     private readonly ILlmClient _llmClient;
+    private readonly IUnitOfWork _unitOfWork;
     
-    public LlmService(ILlmClient llmClient)
+    public LlmService(ILlmClient llmClient, IUnitOfWork unitOfWork)
     {
         _llmClient = llmClient;
+        _unitOfWork = unitOfWork;
     }
     public async Task<string> ProcessPromptAsync(string prompt, CancellationToken cancellationToken)
     {
